4a2073f0e8066ce7244be58e771483482aad764d
Lucas Duarte <lucasduarte599@gmail.com>
2020-04-16 12:49:47 -0300

    commit inicial

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,73 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="app">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link to="/">
+          <v-list-item-action>
+            <v-icon>mdi-calendar</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Calendário</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/evento">
+          <v-list-item-action>
+            <v-icon>mdi-calendar-plus</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Eventos</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/responsavel">
+          <v-list-item-action>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Responsáveis</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link to="/sala">
+          <v-list-item-action>
+            <v-icon>mdi-map-marker</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Salas</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Agenda</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2020</span>
+    </v-footer>
+  </v-app>
 </template>
+<script>
+import Calendario from "./components/Calendario";
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+export default {
+  name: "App",
 
-#nav {
-  padding: 30px;
+  props: {
+    source: String
+  },
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  components: {
+    Calendario
+  },
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+  data: () => ({
+    drawer: null
+  })
+};
+</script>
