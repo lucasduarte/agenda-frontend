fcd5296784cadcf8c7f6c4af2ce5c59bb10c95cb
Lucas Duarte <lucasduarte599@gmail.com>
2020-04-16 13:09:40 -0300

    remove referencia de plugin nao utilizado

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import DatetimePicker from 'vuetify-datetime-picker'
 
 import pt from 'vuetify/es5/locale/pt'
 
 Vue.use(Vuetify);
-Vue.use(DatetimePicker);
 
 export default new Vuetify({
   lang: {
